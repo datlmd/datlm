e043e0f2fc0f3701799bef9927ddd5d8c5f9f437
Dat Le <z.dat.leminh@gree.net>
2018-12-06 16:56:12 +0700

    add template magazine

---
+++ fuel/application/views/_layouts/magazine.php
@@ -0,0 +1,6 @@
+<?php $this->load->view('_blocks/magazine/header')?>
+
+		<?php echo fuel_var('body', 'This is a default layout. To change this layout go to the fuel/application/views/_layouts/magazine.php file.'); ?>
+
+	
+<?php $this->load->view('_blocks/magazine/footer')?>
